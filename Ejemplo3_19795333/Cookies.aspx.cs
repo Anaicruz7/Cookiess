875969ee38f73ea3e4817c8fcf4160374d3e288a
Anaicruz7 <alisoncruz5565@gmail.com>
2025-02-26 16:06:30 -0600

    mi primer commit

---
+++ Ejemplo3_19795333/Cookies.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ejemplo3_19795333
+{
+	public partial class Cookies : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+           Session ["ddlCategory"] = ddlCategory.SelectedValue;
+          Session["ddlSupplier"] =ddlSupplier.SelectedValue  ;
+           Session["txtProduct"] =txtProduct.Text;
+           Session["txtDescripcion"] = txtDescripcion.Text;
+           Session["txtImagen"] = txtImagen.Text;
+            Session["decPrice"] = txtPrice.Text;
+            Session["txtNumberInStock"] = txtNumberInStock.Text;
+            Session["txtNumberOnOrder"] = txtNumberOnOrder.Text;
+            Session["txtReorderLevel"] = txtReorderLevel.Text;
+
+            Response.Redirect("Cookie_Session.aspx");
+        }
+    }
+}
